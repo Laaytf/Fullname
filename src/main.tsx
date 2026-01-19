d4b08bbf8b101565dbe5f7c7a2da3f19255d8ade
Laaytf <Layslathauanne.21@gmail.com>
2026-01-19 00:12:06 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
-import { ErrorBoundary } from './components/error-boundary'
-import { Toaster } from './components/ui/toaster'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-      <Toaster />
-    </ErrorBoundary>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
